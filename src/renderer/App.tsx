f49e1cba151d49a5ea5c34f36213503b4fe5f89e
Rick <rick.kohjiaxuan@gmail.com>
2025-01-05 22:34:46 +0800

    Install shadcn and initialise with button component

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,7 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import './Tailwind.css';
+import { Button } from '@/components/ui/button';
 import Explorer from './components/Explorer';
 import FileProvider from './contexts/FileContext';
 import Viewer from './components/Viewer';
@@ -11,6 +12,9 @@ function Hello() {
     <main className="w-full h-screen overflow-hidden">
       <FileProvider>
         <div className="flex flex-row w-full h-full">
+          <Button onClick={() => console.log('Hello, world!')}>
+            Hello, world!
+          </Button>
           <Explorer />
           <Viewer />
           <Toolbar />
