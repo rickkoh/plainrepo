473ceb851aca53b39dcb7a30021ea708744db7bb
Rick Koh <rick.koh91@titansoft.com.sg>
2025-01-24 13:13:47 +0800

    Change content rendering format

--- src/main/utils/ContentAggregator.ts
+++ src/main/utils/ContentAggregator.ts
@@ -5,7 +5,7 @@ const fs = require('fs');
 export default function getContent(rootFileNode: FileNode) {
   function readContent(fileNode: FileNode): string {
     if (fileNode.type === 'file' && fileNode.selected) {
-      return `${fileNode.name}\n====\n${fs.readFileSync(fileNode.path, 'utf-8')}\n====\n`;
+      return `\`\`\`${fileNode.name}\n${fs.readFileSync(fileNode.path, 'utf-8')}\n\`\`\`\n`;
     }
     if (fileNode.type === 'directory') {
       let content = '';
